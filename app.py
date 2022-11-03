0a87e86484d1bec26fcfb7777d17d0f819fe8b36
amina95anjoom <77480986+amina95anjoom@users.noreply.github.com>
2022-11-03 14:04:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,34 +9,34 @@ import streamlit as st
 from email_generator import email_generator
 
 
+
 st.markdown("# Generate Email")
 
 backend = email_generator()
 
 with st.form(key="form"):
     prompt = st.text_input("Key words")
-   # st.text("(Example: Write me a professional sounding email to my boss)")
 
     start = st.text_input("Begin writing the first few or several words of your email:")
 
-    #slider = st.slider("How many characters do you want your email to be? ", min_value=64, max_value=550)
-    #st.text("(A typical email is usually 100-500 characters)")
+    submit_button1 = st.form_submit_button(label='Generate Email')
 
-    submit_button = st.form_submit_button(label='Generate Email')
-
-    if submit_button:
+    if submit_button1:
         with st.spinner("Generating Email..."):
             output = backend.generate_email(prompt, start)
         st.markdown("# Email Output:")
-        st.subheader(start + output)
+        st.subheader(start + output )
         st.markdown("____")
-       # st.markdown("# Send Your Email")
-        #st.subheader("You can press the Generate Email Button again if you're unhappy with the model's output")
-        
-        #st.subheader("Otherwise:")
         st.text(output)
-       # url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + backend.replace_spaces_with_pluses(start + output)
-
-        #st.markdown("[Click me to send the email]({})".format(url))
-
+    submit_button2 = st.form_submit_button(label='Arabic Translation')
+    if submit_button2:
+        out=output
+        with st.spinner("Generating translation..."):
+            translation = backend.generate_arabic(out)
+        st.markdown("# Email Output:")
+        st.subheader(out)
+        st.markdown("____")
+        st.text(translation)
+    
+     
 
